d7effa86cdf0c6ce996aefef861695ffcc57bbe0
dylan conliffe <dylan conliffe>
2016-08-18 12:01:15 -0600

    ex2

---
+++ ex 2/ex-2.js
@@ -0,0 +1,11 @@
+var why = prompt("Hello!, why are you here");
+ 
+var why = window.prompt(`You entered : ${why}    
+The length of ${why} is : ${why.length}    
+The fifth character is : ${why.charAt(5)}    
+Lowecase : ${why.toLowerCase()}
+Uppercase: ${why.toUpperCase()}
+A sentence using ${why}: ${why} ! that must be  a lie!? pls...be
+sub word: ${why.substr(1,3)}` ); 
+
+

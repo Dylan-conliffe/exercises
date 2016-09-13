99a69072e713e65efbcb5a9c03dd4c5821b53a69
dylan conliffe <dylan conliffe>
2016-09-13 13:15:21 -0600

    part one

---
+++ week-six/week-six-ex/main.js
@@ -0,0 +1,18 @@
+function color(){
+  
+console.log(process.argv)
+
+var r = process.argv[2]
+var g =  process.argv[3]
+var b =  process.argv[4]
+var luminosity = 0.2126*r + 0.7152*g + 0.0722*b 
+if(luminosity > 155){
+    console.log('light')
+}
+else {
+   console.log( "dark")
+}
+
+console.log(luminosity);
+}
+color()
